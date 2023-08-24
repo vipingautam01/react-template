e14c692dcf6dc3a3894f591ca4303b420f73ed6e
Vipin Gautam <115401117+vipsir12@users.noreply.github.com>
2023-08-24 22:18:49 +0530

    component

--- src/module1.mjs
+++
@@ -1,4 +0,0 @@
-// import ui from './module2.mjs'
-// console.log(ui);
-
-// it will print the value of default export from the module2.js 
--- src/module2.mjs
+++
@@ -1,6 +0,0 @@
-// const a = "vipin";
-// const b = "sakshi";
-// const c = "motu";
-// const d = "deepak";
-
-// export default b; 
