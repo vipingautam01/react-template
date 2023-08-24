6407e379958a3c5dcb4b8d4e60fe6fb306fa4e89
Vipin Gautam <115401117+vipsir12@users.noreply.github.com>
2023-08-24 23:22:32 +0530

    props and propTypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">{props.aboutText}</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
@@ -39,4 +42,16 @@ export default function Navbar() {
   </div>
 </nav>
   )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string,
+    aboutText : PropTypes.string
+}
+
+// PropTypes.string.isRequired  - required value for the props 
+
+Navbar.defaultProps = {
+    title : "default title",
+    aboutText:"about text"
 }
